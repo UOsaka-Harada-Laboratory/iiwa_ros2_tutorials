73d116c2319d65d49514f42509c94829d0bd8204
takuya-ki <taku8926@gmail.com>
2025-01-11 18:59:22 +0900

    enabled demo_moveit_simulation.sh

---
+++ utils/demo_moveit_simulation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+byobu new-session -d -s demo_moveit_simulation
+byobu select-pane -t 0
+byobu split-window -v
+byobu select-pane -t 0
+byobu split-window -h
+byobu select-pane -t 2
+byobu split-window -h
+byobu select-pane -t 3
+
+byobu send-keys -t 0 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup mock.launch.py model:=iiwa14"' 'C-m'
+sleep 1.
+byobu send-keys -t 1 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup move_group.launch.py mode:=mock rviz:=true model:=iiwa14"' 'C-m'
+
+byobu attach -t demo_moveit_simulation
