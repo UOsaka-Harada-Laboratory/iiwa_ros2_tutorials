aea474240efe358b61c4a41ed7de24bd0ebb8dc7
takuya-ki <taku8926@gmail.com>
2025-05-06 12:05:37 +0900

    update accordingly

--- utils/demo_moveit_hardware_gui.sh
+++ utils/demo_moveit_hardware_gui.sh
@@ -5,7 +5,7 @@
 # FRI control mode: POSITION_CONTROL
 # FRI client command mode: POSITION
 
-byobu new-session -d -s demo_moveit_hardware_py
+byobu new-session -d -s demo_moveit_hardware_gui
 byobu select-pane -t 0
 byobu split-window -v
 byobu select-pane -t 0
@@ -18,10 +18,8 @@ byobu send-keys -t 0 'xhost + && docker exec -it iiwa_ros2_container bash -it -c
 sleep 1.
 byobu send-keys -t 1 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "cp --force /yaml/lbr_controllers_moveit.yaml /root/ros2_ws/src/lbr-stack/src/lbr_fri_ros2_stack/lbr_description/ros2_control/lbr_controllers.yaml"' 'C-m'
 sleep 1.
-byobu send-keys -t 0 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup hardware.launch.py moveit:=true model:=iiwa14 sim:=true base_frame:=world"' 'C-m'
-sleep 2.
-byobu send-keys -t 1 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup move_group.launch.py mode:=hardware model:=iiwa14"' 'C-m'
-sleep 5.
-byobu send-keys -t 2 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_moveit_py hello_moveit.launch.py mode:=hardware model:=iiwa14"' 'C-m'
+byobu send-keys -t 0 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup hardware.launch.py model:=iiwa14"' 'C-m'
+sleep 3.
+byobu send-keys -t 1 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup move_group.launch.py mode:=hardware rviz:=true model:=iiwa14"' 'C-m'
 
-byobu attach -t demo_moveit_hardware_py
+byobu attach -t demo_moveit_hardware_gui
