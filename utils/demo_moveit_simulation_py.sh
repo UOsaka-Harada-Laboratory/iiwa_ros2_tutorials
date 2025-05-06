174ead3e3e539c73fd262c63351125dad814b4a5
takuya-ki <taku8926@gmail.com>
2025-05-06 11:59:54 +0900

    add a launch file for demo_moveit_simulation_py.sh

--- utils/demo_moveit_simulation_py.sh
+++ utils/demo_moveit_simulation_py.sh
@@ -13,6 +13,6 @@ byobu send-keys -t 0 'xhost + && docker exec -it iiwa_ros2_container bash -it -c
 sleep 2.
 byobu send-keys -t 1 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_bringup move_group.launch.py mode:=mock rviz:=true model:=iiwa14"' 'C-m'
 sleep 5.
-byobu send-keys -t 2 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 run lbr_moveit_py hello_moveit --ros-args -r __ns:=/lbr"' 'C-m'
+byobu send-keys -t 2 'xhost + && docker exec -it iiwa_ros2_container bash -it -c "ros2 launch lbr_moveit_py hello_moveit.launch.py mode:=mock model:=iiwa14"' 'C-m'
 
 byobu attach -t demo_moveit_simulation_py
